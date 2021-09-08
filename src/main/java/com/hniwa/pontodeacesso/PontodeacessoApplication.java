09a5b74daa04ce312def215b17d6fac67d7cabd9
Henrique Niwa <z.github1@mailx.win>
2021-09-08 16:24:23 -0300

    Initial commit

---
+++ src/main/java/com/hniwa/pontodeacesso/PontodeacessoApplication.java
@@ -0,0 +1,13 @@
+package com.hniwa.pontodeacesso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontodeacessoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontodeacessoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hniwa/pontodeacesso/PontodeacessoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hniwa.pontodeacesso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontodeacessoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
